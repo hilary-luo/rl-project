3ef4fd51b8da0abec9d37fc0c02b0d4125c41604
Hilary Luo <103377417+hilary-luo@users.noreply.github.com>
2023-04-08 23:21:36 -0400

    Conventional method fixed

--- cas726_project/cas726_project/frontier_explorer.py
+++ cas726_project/cas726_project/frontier_explorer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import rclpy
+import math
 
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -11,23 +12,20 @@ from nav2_msgs.action import NavigateToPose
 from skimage import measure
 from skimage.measure import label, regionprops
 
+MIN_REGION_AREA = 20
+
 class Frontier_Explorer(Node):
     def __init__(self):
         # Initialize ROS node
         super().__init__('frontier_explorer')
 
-
         # Set up publishers, subscribers and clients
         self.nav2_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
         self.map_subscriber = self.create_subscription(OccupancyGrid, '/map', self.map_callback, 1)
         self.pose_subscriber = self.create_subscription(PoseWithCovarianceStamped, '/pose', self.pose_callback, 1)
-        self.pose_subscriber = self.create_subscription(OccupancyGrid, '/global_costmap/costmap', self.costmap_callback, 1)
-
-
-        self.publisher_free = self.create_publisher(OccupancyGrid, '/free_cells', 10)
-        self.publisher_unknown = self.create_publisher(OccupancyGrid, '/unknown_cells', 10)
+        self.publisher_free = self.create_publisher(OccupancyGrid, '/free_cell_mask', 10)
+        self.publisher_unknown = self.create_publisher(OccupancyGrid, '/unknown_cell_mask', 10)
         self.publisher_frontier = self.create_publisher(OccupancyGrid, '/frontier_cells', 10)
-
 
         # Initialize environment variables
         self.map_msg = None
@@ -36,6 +34,8 @@ class Frontier_Explorer(Node):
         self.frontier_list = list()
         self.print = False
 
+        self.start_time = self.get_clock().now()
+
         # Wait for navigation to fully activate
         print('Waiting for Nav2 server')
         self.nav2_client.wait_for_server()
@@ -43,12 +43,7 @@ class Frontier_Explorer(Node):
         print('Frontier Explorer Node initialized')
 
     def find_frontiers(self):
-        # if (self.print):
-        #     print(f"Inside find frontiers: {self.get_map()}")
         map = self.get_map()
-        #print(f"Map min: {np.min(map)}, max: {np.max(map)}")
-        list_temp = map.flatten().tolist()
-        #print(f"List min: {min(list_temp)}, max: {max(list_temp)}")
 
         # Detect Frontiers in Free Cells
         free_map = (np.logical_and((map < 10), (map >= 0))).astype(int) # '1' are the open cells, '0' are the unknown or occupied cells
@@ -78,37 +73,36 @@ class Frontier_Explorer(Node):
         unknown_map_msg.data = (unknown_map.flatten()*100).tolist()
         self.publisher_unknown.publish(unknown_map_msg)
 
-        # Cross check between the frontiers to find the frontiers for exploration
+        # Cross check between the frontiers to find the frontiers for exploration (ignore frontiers between free and occupied and unknown and occupied)
         frontier_cells = [c for c in unknown_cells if c in free_cells]
 
         frontier_map = np.zeros(map.shape)
         for cell in frontier_cells:
             frontier_map[int(cell[0]), int(cell[1])] = 1
 
-        frontier_map_msg = OccupancyGrid()
-        frontier_map_msg.header = self.map_msg.header
-        frontier_map_msg.info = self.map_msg.info
-        frontier_map_msg.data = (frontier_map.flatten()*100).astype(int).tolist()
-        self.publisher_frontier.publish(frontier_map_msg)
-
         # Cluster cells into frontiers
-        labeled_frontier_map, label_num = measure.label(frontier_map, return_num=True)
+        labeled_frontier_map = measure.label(frontier_map)
         map_origin_x, map_origin_y = self.get_map_origin()
 
+        # Populate the list of all frontiers to be considered and the required metrics
         self.frontier_list.clear()
         for region in regionprops(labeled_frontier_map):
-            if region.area >= 20:
+            if region.area >= MIN_REGION_AREA:
                 # Find the centre of the frontier
-                x = region.centroid[1]
-                y = region.centroid[0]
+                x = region.centroid[1]*self.map_msg.info.resolution+map_origin_x
+                y = region.centroid[0]*self.map_msg.info.resolution+map_origin_y
                 pose_x, pose_y = self.get_pose()
-                self.frontier_list.append( # Tuple of Manhatten distance, x, y, size
-                    (abs(x - pose_x) + abs(y - pose_y), 
+                self.frontier_list.append( # List of tuples of (Distance, size, centroid x, centroid y, label)
+                    (math.sqrt((x - pose_x)**2 + (y - pose_y)**2), 
                      region.area, 
-                     x*self.map_msg.info.resolution+map_origin_x,
-                     y*self.map_msg.info.resolution+map_origin_y)) 
-                self.frontier_list.sort(reverse=True)
+                     x,
+                     y,
+                     region.label)) 
+                
+        self.publish_frontiers(labeled_frontier_map)
         print('Frontier count: ',len(self.frontier_list))
+
+    # Getters
 
     def get_map(self):
         return np.array(self.map_msg.data, dtype=np.int8).reshape(self.map_msg.info.height, self.map_msg.info.width)
@@ -119,18 +113,39 @@ class Frontier_Explorer(Node):
     def get_pose(self):
         return (self.pose_msg.pose.pose.position.x, self.pose_msg.pose.pose.position.y)
 
+    def publish_frontiers(self, labeled_frontier_map):
+        labeled_map_np = np.array(labeled_frontier_map)
+        processed_map = np.zeros(labeled_map_np.shape)
+        n = 0
+        local_list = self.frontier_list.copy()
+        local_list.sort(reverse=True) # Order so pink will be the target
+        for frontier in local_list:
+            print(frontier)
+            print(f'Distance is {frontier[0]}, Label is {frontier[4]}, n is {n}')
+            n = n + 1
+            processed_map[labeled_map_np == frontier[4]] = n
+
+        frontier_map_msg = OccupancyGrid()
+        frontier_map_msg.header = self.map_msg.header
+        frontier_map_msg.info = self.map_msg.info
+        frontier_map_msg.data = ((processed_map*100/n).flatten()).astype(int).tolist()
+        self.publisher_frontier.publish(frontier_map_msg)
+
+
     def select_frontier(self):
-        # 
-        ''
-    
-    def go_to_frontier(self):
+        # TODO: Choose the frontier in a more effective way (currently going to closest)
+        self.frontier_list.sort()
+        selected_frontier = self.frontier_list[0]
+        return selected_frontier
+
+    def go_to_frontier(self, frontier):
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
-        goal_pose.pose.position.x = self.frontier_list[0][2]
-        goal_pose.pose.position.y = self.frontier_list[0][3]
+        goal_pose.pose.position.x = frontier[2]
+        goal_pose.pose.position.y = frontier[3]
         goal_pose.pose.orientation.z = 0.0
 
-        print(f"Selected Frontier Pose is {goal_pose.pose.position.x:.2f}, {goal_pose.pose.position.y:.2f} at distance {self.frontier_list[0][0]} and of size {self.frontier_list[0][1]}")
+        print(f"Selected Frontier Pose is {goal_pose.pose.position.x:.2f}, {goal_pose.pose.position.y:.2f} at distance {frontier[0]} and of size {frontier[1]}")
 
         # Send the goal pose to the navigation stack
         print('Sending the pose')
@@ -158,7 +173,7 @@ class Frontier_Explorer(Node):
     def check_done(self):
         perc_pixels_visited = np.sum((np.array(self.map_msg.data) >= 0).astype(int)) / len(self.map_msg.data)
         print(f'Visited {perc_pixels_visited*100:.1f}% of pixels\n')
-        return bool(perc_pixels_visited > 0.60)
+        return (bool(perc_pixels_visited > 0.60), self.start_time.nanoseconds, self.get_clock().now().nanoseconds)
 
     # Callbacks
 
@@ -197,13 +212,16 @@ def main(args=None):
 
     while not done:
         frontier_explorer.find_frontiers()
-        frontier_explorer.select_frontier()
-        if(len(frontier_explorer.frontier_list)>0):
-            print(frontier_explorer.frontier_list)
-            frontier_explorer.go_to_frontier()
-            done = frontier_explorer.check_done()
-        rclpy.spin_once(frontier_explorer)
+        frontier = frontier_explorer.select_frontier()
+        if(frontier is not None):
+            #print(frontier_explorer.frontier_list)
+            frontier_explorer.go_to_frontier(frontier)
+            done, start_time, curr_time = frontier_explorer.check_done()
+        rclpy.spin_once(frontier_explorer) # Allows callbacks to update the map
         
+    duration = curr_time - start_time
+
+    print(f'Exploration took {duration/1e9} seconds')
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
